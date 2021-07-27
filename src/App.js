7c4943d1e906b1fcbd728dfcf1bcbb50a0a0b22a
CHEREDDYHARIKRISHNA <chereddyharikrishna781@gmail.com>
2021-07-27 17:44:43 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React,{Component}from 'react'
+import './App.css';
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+export default class App extends Component {
+     constructor(props){
+       super(props);
+       this.state={
+          todos:[{id:1, title:"first todo",complete:false},
+               {id:2,title:"second todo",complete:false},
+               {id:3,title:"third todo",complete:false},
+              ],
+       };
+     }
+        
+     addTodo =(title)=>{
+       console.log( title);
+        let todos=[...this.state.todos];
+        let id=[todos.length-1]?[todos.length-1]['id']+1:'0';
+        let newTodo ={
+          id,
+          title,
+          complete:false
+        };
+        todos.push(newTodo);
+        this.setState({todos});
+         
+      };
+      completeTodo= (id) =>{
+        let todos=[...this.state.todos];
+        todos.filter((todo)  =>  {
+          if(todo.id===id){
+           todo.complete=true;
+          }
+      });
+this.setState({todos});
+      };
+      deleteTodo=(id)=>{
+        let todos=[...this.state.todos];
+         todos.filter((todo,index)=>{
+           if(todo.id===id){
+             todos.splice(index,1);
+           }
+         });
+         this.setState({todos});
+
+      };
+  render(){
+  return (
+    <div>
+    <div className='developer'>
+      <span>developer</span>
+      <br/>
+
+      <b>harikrishna chereddy</b>
+      
+
+
+    </div>
+     <h1 className="heading">todo management application with React</h1>
+     <div className="todos">
+     {this.state.todos.map((todo)=>(
+     <Todo key={todo.id}todo={todo} completeTodo={(id)=>this.completeTodo(id)}
+     deleteTodo={(id)=>this.deleteTodo(id)}
+     /> ))}
+    
+     <TodoForm addTodo={(todo)=>this.addTodo(todo)}/>
+    </div>
+    </div>
+  );
+  }
+}
+
---
+++ todoapp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '/App.css';
+function App() {
+  return (
+    <div >
+      <h1 className="heading">todo management application with React</h1>
+    </div>
+  );
+}
+
+export default App;
